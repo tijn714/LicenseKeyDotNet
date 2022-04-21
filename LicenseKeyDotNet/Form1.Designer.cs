a4a04c44bd73ed3d31d697afa7a8f25feb5c4640
Tijn Rodrigo <92756641+Tijn-Rodrigo@users.noreply.github.com>
2022-04-21 14:24:49 +0200

    Add files via upload

---
+++ LicenseKeyDotNet/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace LicenseKeyDotNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subcribersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.activateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.abouytToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_secret = new System.Windows.Forms.Button();
+            this.label_status = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(476, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.subcribersToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // subcribersToolStripMenuItem
+            // 
+            this.subcribersToolStripMenuItem.Name = "subcribersToolStripMenuItem";
+            this.subcribersToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
+            this.subcribersToolStripMenuItem.Text = "&Subcribers";
+            this.subcribersToolStripMenuItem.Click += new System.EventHandler(this.subcribersToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(170, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
+            this.quitToolStripMenuItem.Text = "&Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.activateToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.abouytToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.aboutToolStripMenuItem.Text = "&Other";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.helpToolStripMenuItem.Text = "&Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // activateToolStripMenuItem
+            // 
+            this.activateToolStripMenuItem.Name = "activateToolStripMenuItem";
+            this.activateToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.activateToolStripMenuItem.Text = "Activate";
+            this.activateToolStripMenuItem.Click += new System.EventHandler(this.activateToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(143, 6);
+            // 
+            // abouytToolStripMenuItem
+            // 
+            this.abouytToolStripMenuItem.Name = "abouytToolStripMenuItem";
+            this.abouytToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.abouytToolStripMenuItem.Text = "&About";
+            this.abouytToolStripMenuItem.Click += new System.EventHandler(this.abouytToolStripMenuItem_Click);
+            // 
+            // btn_secret
+            // 
+            this.btn_secret.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn_secret.Location = new System.Drawing.Point(163, 129);
+            this.btn_secret.Name = "btn_secret";
+            this.btn_secret.Size = new System.Drawing.Size(123, 44);
+            this.btn_secret.TabIndex = 1;
+            this.btn_secret.Text = "Secret";
+            this.btn_secret.UseVisualStyleBackColor = false;
+            this.btn_secret.Click += new System.EventHandler(this.btn_secret_Click);
+            // 
+            // label_status
+            // 
+            this.label_status.Location = new System.Drawing.Point(12, 280);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(452, 23);
+            this.label_status.TabIndex = 3;
+            this.label_status.Text = "Status:";
+            this.label_status.Click += new System.EventHandler(this.label_status_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 312);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.btn_secret);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "License.NET";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subcribersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem activateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem abouytToolStripMenuItem;
+        private System.Windows.Forms.Button btn_secret;
+        private System.Windows.Forms.Label label_status;
+    }
+}
+
---
+++ LicenseKeyDotNet/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QP = QP_Helpers.QP_Helpers;
+
+namespace LicenseKeyDotNet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            string fullRegistryPath = @"HKEY_CURRENT_USER\Software\DotNetLicense";
+            string registryValueName = "KeY";
+
+            string key = QP.GetRegister(fullRegistryPath, registryValueName);
+
+            if (key != null && QP.IsLicensed(key, "LicenseKeyDotNet.LicenseKeys.txt"))
+                QP._isLicensed = true;
+            else
+            {
+                QP._isLicensed = false;
+
+                Window_Buy win = new Window_Buy();
+
+                win.ShowDialog();
+            }
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (QP._isLicensed == false)
+            {
+                subcribersToolStripMenuItem.Enabled = false;
+                btn_secret.Enabled = false;
+                label_status.Text = "Status: No License key found."; 
+
+            }
+
+            else
+            {
+                activateToolStripMenuItem.Enabled = false;
+                activateToolStripMenuItem.Text = "Activated";
+                label_status.Text = "Status: License Active";
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void subcribersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LicenseKeyDotNet.sub per = new LicenseKeyDotNet.sub();
+            per.Show();
+        }
+
+        private void activateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (QP._isLicensed==true)
+            {
+                activateToolStripMenuItem.Enabled = false;
+                activateToolStripMenuItem.Text = "Activated";
+
+            }
+            else
+            {
+                Window_Buy act = new Window_Buy();
+
+                act.ShowDialog();
+            }
+        }
+
+        private void abouytToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LicenseKeyDotNet.about abt = new LicenseKeyDotNet.about();
+            abt.Show();
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Q: I activated the app with the menu but everything is still disabled, how to fix? \nA: Restart the app to activate all the functions.\n\n"
+                           +"Q: Why is the 'activate' in the menu disabled? \nA: You already activated the software, so you don't need this :)" ,"Help");
+        }
+
+        private void btn_secret_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I am proud of you.","Secret");
+        }
+
+        private void label_status_Click(object sender, EventArgs e)
+        {
+            if (QP._isLicensed == true)
+            {
+                label_status.Text = "Status: License Active";
+            }
+            else
+            {
+                label_status.Text = "Status: No License key found.";
+            }
+        }
+    }
+}
